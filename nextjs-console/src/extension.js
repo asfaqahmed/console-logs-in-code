6b7e9b8ce2d991fb201456a2399422cb62aa0630
Asfaq Ahmed <asfaqahmed356@gmail.com>
2024-10-22 16:21:06 +0530

    add-extension-ts

---
+++ nextjs-console/src/extension.js
@@ -0,0 +1,40 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.activate = activate;
+exports.deactivate = deactivate;
+const vscode = __importStar(require("vscode"));
+function activate(context) {
+    // Create a new output channel named 'Next.js Console'
+    const outputChannel = vscode.window.createOutputChannel('Next.js Console');
+    outputChannel.show();
+    // Register a command to display a welcome message
+    let disposable = vscode.commands.registerCommand('nextjs-console.showWelcome', () => {
+        outputChannel.appendLine('Next.js Console is Active!');
+        vscode.window.showInformationMessage('Next.js Console is Active!');
+    });
+    context.subscriptions.push(disposable);
+}
+function deactivate() { }
